aac0cc438a8a80c5df20cb94b13f5b6d6f8f0a80
jimmy-yc-lin <okesseko@gmail.com>
2023-09-22 18:11:51 +0800

    feat: add bnt to send event

--- popup.js
+++ popup.js
@@ -1,17 +1,27 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const injectButton = document.getElementById("injectButton");
+  var dButtonEvent = document.getElementById("btn1");
+  var dButtonContent = document.getElementById("btn2");
 
-  injectButton.addEventListener("click", function () {
-    const codeToInject = "const a = 1;";
-    chrome.devtools.inspectedWindow.eval(
-      codeToInject,
-      function (result, isException) {
-        if (isException) {
-          console.error("Error injecting code:", result);
-        } else {
-          console.log("Code injected successfully:", result);
-        }
+  //點擊按鈕，向事件腳本發送訊息
+  dButtonEvent.addEventListener("click", function (ce) {
+    chrome.runtime.sendMessage(
+      { content: "你好，此訊息來自彈出視窗腳本" },
+      function (response) {
+        console.log(response);
       }
     );
+  });
+
+  //點擊按鈕，向內容腳本發送訊息
+  dButtonContent.addEventListener("click", function (ce) {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      chrome.tabs.sendMessage(
+        tabs[0].id,
+        { content: "你好，此訊息來自彈出視窗腳本" },
+        function (response) {
+          console.log(response);
+        }
+      );
+    });
   });
 });
