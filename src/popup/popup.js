d0fb5c84c49b28e8f5e8c4f65602e0bacfb0bc10
jimmy-yc-lin <okesseko@gmail.com>
2023-09-24 22:35:20 +0800

    feat: get custom ic card form value

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -12,13 +12,42 @@ const addClickEventToBtn = (btnId) => {
     .addEventListener("click", () => sendEventToContentScript(btnId));
 };
 
+const setCustomIcCardForm = () => {
+  const customIcCardForm = document.getElementById("custom-iccard-form");
+  customIcCardForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const GetRegisterBasicErrorCode = document.getElementById(
+      "GetRegisterBasic-errorCode"
+    );
+    const GetRegisterBasicPersonalId = document.getElementById(
+      "GetRegisterBasic-personalId"
+    );
+    const GetSeqNumber256N1ErrorCode = document.getElementById(
+      "GetSeqNumber256N1-errorCode"
+    );
+    const GetTreatNumNoIcCardErrorCode = document.getElementById(
+      "GetTreatNumNoIcCard-errorCode"
+    );
+
+    console.log(
+      GetRegisterBasicErrorCode.value,
+      GetRegisterBasicPersonalId.value,
+      GetSeqNumber256N1ErrorCode.value,
+      GetTreatNumNoIcCardErrorCode.value
+    );
+  });
+};
+
 document.addEventListener("DOMContentLoaded", function () {
   const btnIdList = [
     "mock-emr",
     "mock-emr-cancel",
     "mock-iccard-current",
-    "mock-iccard-custom",
+    // "mock-iccard-custom",
     "mock-iccard-cancel",
   ];
   btnIdList.forEach((id) => addClickEventToBtn(id));
+
+  setCustomIcCardForm();
 });
